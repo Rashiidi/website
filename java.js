c1282963c93be5cd0e1cde4655b59fb9251e5182
Rashka <abdirashid@gmail.com>
2024-10-16 09:37:02 +0300

    added links

--- java.js
+++ java.js
@@ -1,2 +1,11 @@
-
-window.slide = new SlideNav();
+function toggleContent(section) {
+  var content = document.getElementById(section + "-content");
+  var button = document.getElementById(section + "-button");
+  if (content.style.display === "none") {
+      content.style.display = "block";
+      button.textContent = "Read Less";
+  } else {
+      content.style.display = "none";
+      button.textContent = "Read More";
+  }
+}
